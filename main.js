a4f4e1ca4aa4239de5ae705aa5adf51351a44277
titas.partikas <titas.partikas@devbridge.com>
2022-12-27 17:16:15 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,27 @@
+// TASK 1
+// Create an object car with these fields: brand, color, numOfDoors. Fill those fields with values and console.log each of them separately.
+
+
+
+// TASK 2
+// Create a function createUser that takes first name and last name, and returns object with same named keys and its values are set to the passed arguments.
+
+
+
+// TASK 2.1
+// Add a method to that object that when it's invoked it should return full name of the user, for example 'Vardenis Pavardenis'.
+
+
+
+// TASK 3
+// Create two inputs and a button in your HTML, when button is clicked create and return an object with its key set as the first input value and its value is set to the second input value.
+
+
+
+// TASK 4
+// Create a function isEqual that takes two objects and returns if they are equal. Use function created in second task to create objects that you need to compare. Remember functions are reference type just like objects are.
+
+
+
+// BONUS TASK
+// Demonstrate closures in javascript.
